f763c770a3e8188a7261f8f2a17b93ebdc6a815a
Remco van der Velde <remco.pc@outlook.com>
2022-01-28 19:28:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ echo "chown www-data:www-data /Application/Data -R" >> ~/.bashrc
 echo "cp /Application/vendor/r3m/framework/Bin/r3m.php /Application/Bin/R3m.php" >> ~/.bashrc
 echo "php /Application/Bin/R3m.php bin r3m.io" >> ~/.bashrc
 echo "r3m.io configure environment development" >> ~/.bashrc
+echo "r3m.io ln /Application/Host/Script/Universeorange/Com /Application/Host/Script/Universeorange/Local" >> ~/.bashrc
+echo "r3m.io ln /Application/Public/Js/Development/Priya /Application/Host/Script/Universeorange/Local/Public/Js/Priya" >> ~/.bashrc
 echo "chown www-data:www-data /Application/Data -R" >> ~/.bashrc
 echo "r3m.io configure host add 0.0.0.0 script.universeorange.local" >> ~/.bashrc
 echo "r3m.io info all" >> ~/.bashrc
