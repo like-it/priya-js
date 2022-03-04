e6c8d3814f0e9ed991b1a1ded0147dc750a167fa
Remco van der Velde <remco.pc@outlook.com>
2022-03-04 14:54:49 +0100

    missing files

---
+++ Public/Js/Development/Priya/Bin/Zzz.object.prototype.js
@@ -0,0 +1,7 @@
+_('prototype').object = {};
+_('prototype').object.delete = _('prototype').object_delete;
+_('prototype').object.get = _('prototype').object_get;
+_('prototype').object.horizontal = _('prototype').object_horizontal;
+_('prototype').object.merge = _('prototype').object_merge;
+_('prototype').object.set = _('prototype').object_set;
+priya.object = _('prototype').object;
